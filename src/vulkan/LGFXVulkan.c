f0787fb46eacc29bdd728449acf4a78affbc29f5
Christian <97773844+esuriru@users.noreply.github.com>
2025-02-12 20:28:59 +0800

    Revert "Change `NULL` to 0 for flags"
    This reverts commit 8bc4665113043e6ba0e426cb76919dfb943b4c19.

--- src/vulkan/LGFXVulkan.c
+++ src/vulkan/LGFXVulkan.c
@@ -504,7 +504,7 @@ void VkLGFXEndTemporaryCommandBuffer(LGFXDevice device, LGFXCommandBuffer buffer
 	VkSubmitInfo submitInfo = {0};
     submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
     submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = (VkCommandBuffer*)(&buffer->cmdBuffer);
+    submitInfo.pCommandBuffers = &(VkCommandBuffer)buffer->cmdBuffer;
 
 	//LGFXFence tempFence = VkLGFXCreateFence(device, false);
 
@@ -540,7 +540,7 @@ LGFXFence VkLGFXCreateFence(LGFXDevice device, bool signalled)
 	VkFenceCreateInfo fenceCreateInfo = {0};
 	fenceCreateInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
 	fenceCreateInfo.pNext = NULL;
-	fenceCreateInfo.flags = signalled ? VK_FENCE_CREATE_SIGNALED_BIT : 0;
+	fenceCreateInfo.flags = signalled ? VK_FENCE_CREATE_SIGNALED_BIT : NULL;
 
 	VkFence fence;
 	if (vkCreateFence((VkDevice)device->logicalDevice, &fenceCreateInfo, NULL, &fence) != VK_SUCCESS)
@@ -758,7 +758,7 @@ void VkLGFXGetQueueCreateInfos(VkLGFXQueueProperties *properties, VkPhysicalDevi
 	info->queueFamilyIndex = (u32)properties->dedicatedGraphicsQueueIndex;
 	info->pQueuePriorities = nilPriorityTodo;
 	info->pNext = NULL;
-	info->flags = 0;
+	info->flags = NULL;
 	*outInfoCount += 1;
 
 	if (properties->dedicatedComputeQueueIndex != properties->dedicatedGraphicsQueueIndex)
@@ -769,7 +769,7 @@ void VkLGFXGetQueueCreateInfos(VkLGFXQueueProperties *properties, VkPhysicalDevi
 		info->queueFamilyIndex = (u32)properties->dedicatedComputeQueueIndex;
 		info->pQueuePriorities = nilPriorityTodo;
 		info->pNext = NULL;
-		info->flags = 0;
+		info->flags = NULL;
 		*outInfoCount += 1;
 	}
 	if (properties->dedicatedTransferQueueIndex != properties->dedicatedGraphicsQueueIndex)
@@ -780,7 +780,7 @@ void VkLGFXGetQueueCreateInfos(VkLGFXQueueProperties *properties, VkPhysicalDevi
 		info->queueFamilyIndex = (u32)properties->dedicatedTransferQueueIndex;
 		info->pQueuePriorities = nilPriorityTodo;
 		info->pNext = NULL;
-		info->flags = 0;
+		info->flags = NULL;
 		*outInfoCount += 1;
 	}
 }
@@ -1856,7 +1856,7 @@ LGFXRenderTarget VkLGFXCreateRenderTarget(LGFXDevice device, LGFXRenderTargetCre
 	createInfo.layers = 1;
 	createInfo.attachmentCount = info->texturesCount;
 	createInfo.pAttachments = views;
-	createInfo.flags = 0;
+	createInfo.flags = NULL;
 	createInfo.renderPass = (VkRenderPass)info->forRenderProgram->handle;
 
 	VkFramebuffer frameBuffer;
@@ -3020,7 +3020,7 @@ void VkLGFXCommandBufferBegin(LGFXCommandBuffer buffer, bool resetAfterSubmissio
 	beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 	beginInfo.pInheritanceInfo = NULL;
 	beginInfo.pNext = NULL;
-	beginInfo.flags = resetAfterSubmission ? VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT : 0;
+	beginInfo.flags = resetAfterSubmission ? VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT : NULL;
 
 	if (vkBeginCommandBuffer((VkCommandBuffer)buffer->cmdBuffer, &beginInfo) != VK_SUCCESS)
 	{
